6c5852af0c173a1b74e5d476e8162ee6c0087f5b
Rick Mark <rickmark@outlook.com>
2026-06-07 00:35:23 -0700

    Image captioning system

--- hedonism/who_dis/worker.py
+++ hedonism/who_dis/worker.py
@@ -6,16 +6,81 @@ import numpy as np
 import json
 from gql import Client, gql
 from gql.transport.aiohttp import AIOHTTPTransport
+from transformers import pipeline
 
 API_KEY = "your_api_key_here"
 
-# Select your transport with a defined url endpoint
-GRAPH_TRANSPORT = AIOHTTPTransport(url="http://localhost:5000/graphql", headers={"Authorization": f"Bearer {API_KEY}"})
-
-# Create a GraphQL client using the defined transport
-GRAPH_CLIENT = Client(transport=GRAPH_TRANSPORT)
-
 app = Celery('hedonism_who_dis', broker='redis://default@127.0.0.1:6379/0', result_backend='redis://default@127.0.0.1:6379/0')
+
+PROMPT = "Write a long descriptive caption for this image in a formal tone."
+pipe = pipeline("image-text-to-text", model="llava-hf/llava-1.5-7b-hf")
+
+def graph_client():
+    # Select your transport with a defined url endpoint
+    graph_transport = AIOHTTPTransport(url="http://localhost:5000/graphql",
+                                       timeout=300,
+                                       headers={"Authorization": f"Bearer {API_KEY}"})
+
+    # Create a GraphQL client using the defined transport
+    return Client(transport=graph_transport, execute_timeout=300)
+
+
+def get_photo_url(photo_id):
+
+    # Provide a GraphQL query
+    query = gql(
+        """
+        query FacialRecognitionPhoto($photoId: ID!) {
+          photo(id: $photoId) {
+            id
+            facialRecognitionUrl
+          }
+        }
+    """
+    )
+
+    query.variable_values = {"photoId": photo_id}
+
+    result = graph_client().execute(query)
+    return result['photo']['facialRecognitionUrl']
+
+@app.task
+def caption_image(photo_id):
+    photo_url = get_photo_url(photo_id)
+
+    # Build the conversation
+    captioning_context = [
+        {
+            "role": "system",
+            "content": "You are a helpful image captioner.",
+        },
+        {
+            "role": "user",
+            "content": [
+                {"type": "image", "url": photo_url},
+                {"type": "text", "text": "Describe the contents of this image in a professional way optimized for search"},
+            ],
+        },
+    ]
+    caption_result = pipe(text=captioning_context)
+    caption = caption_result[0]['generated_text'][2]['content']
+    print(f"Generated caption: {caption}")
+
+    caption_update = gql(
+    """
+    mutation CaptionPhoto($photoId: ID!, $caption: String!) {
+      photoCaptionUpdate(id: $photoId, caption: $caption) {
+        photo {
+          id
+        }
+      }
+    }
+    """
+    )
+
+    caption_update.variable_values = { "photoId": photo_id, "caption": caption }
+    result = graph_client().execute(caption_update)
+
 
 @app.task
 def cluster_faces(date_grouping):
@@ -45,31 +110,15 @@ def cluster_faces(date_grouping):
 
 @app.task
 def extract_facial_data(photo_id):
-    # Provide a GraphQL query
-    query = gql(
-        """
-        query FacialRecognitionPhoto($photoId: ID!) {
-          photo(id: $photoId) {
-            id
-            facialRecognitionUrl
-          }
-        }
-    """
-    )
-
-    query.variable_values = {"photoId": photo_id}
-
-    result = GRAPH_CLIENT.execute(query)
-
-    print(f"Source Photo => {result}")
+    photo_url = get_photo_url(photo_id)
 
     # Extract embeddings for all faces found in the image
     try:
         embedding_objects = DeepFace.represent(
-            img_path=result['photo']['facialRecognitionUrl'],
+            img_path=photo_url,
+            enforce_detection=False,
             model_name="ArcFace",         # Alternatives: "VGG-Face", "ArcFace", "OpenFace"
-            detector_backend="retinaface",
-            enforce_detection=True
+            detector_backend="retinaface"
         )
         print(f"Extracted {embedding_objects}")
 
@@ -94,7 +143,8 @@ def extract_facial_data(photo_id):
         ]
 
         embedding_update.variable_values = {"photoId": photo_id, "faceObjects": mapped_faces}
-        result = GRAPH_CLIENT.execute(embedding_update)
+        # Create a GraphQL client using the defined transport
+        result = graph_client().execute(embedding_update)
         print(f"Result of Mutation => {result}")
 
     except Exception as e:
